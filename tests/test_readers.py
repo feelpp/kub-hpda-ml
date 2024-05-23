4ea3b298103afabf856ebde736da91374a65f148
Javier Cladellas <javiercladellas@gmail.com>
2024-05-23 15:06:30 +0200

    fix test

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -4,9 +4,8 @@ from feelpp.ktirio.hpdaml.readers import *
 class TestEnsightReader:
     filepath = "src/cases/case_reader/basic.case"
     ensight_reader = EnsightReader()
-    ensight_reader.readCase(filepath)
     
     def test_getTimeset(self):
-        reader = self.ensight_reader.readCase()
+        reader = self.ensight_reader.readCase(self.filepath)
         time, timesteps = self.ensight_reader.getTimeset()
         assert timesteps == 1
